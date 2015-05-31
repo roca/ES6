ec588fe357547db930c4d7c8fa002fb0073fb49e
Romel Campbell <romel.campbell@regeneron.com>
2015-05-31 05:10:26 -0400

    master #comment completes module 3 on classes

--- starter_with_traceur/app/es5/inheritance.js
+++ starter_with_traceur/app/es5/inheritance.js
@@ -55,7 +55,7 @@ System.registerModule("../es6/inheritance.js", [], function() {
         }
         return ($traceurRuntime.createClass)(Employee, {
           doWork: function() {
-            return (this.name + " is working " + this.title);
+            return (this.name + " is a working " + this.title);
           },
           set title(newValue) {
             if (newValue) {
@@ -70,10 +70,72 @@ System.registerModule("../es6/inheritance.js", [], function() {
       var e1 = new Employee("Developer", "Scott");
       expect(e1.name).toBe("Scott");
       expect(e1.title).toBe("Developer");
-      expect(e1.doWork()).toBe("Scott is working Developer");
+      expect(e1.doWork()).toBe("Scott is a working Developer");
       e1.title = "Manager";
       e1.name = "Christopher";
-      expect(e1.doWork()).toBe("Christopher is working Manager");
+      expect(e1.doWork()).toBe("Christopher is a working Manager");
+    });
+    it('can overide methods in super class', function() {
+      var Person = (function() {
+        function Person(name) {
+          this.name = name;
+        }
+        return ($traceurRuntime.createClass)(Person, {
+          set name(newValue) {
+            if (newValue) {
+              this._name = newValue;
+            }
+          },
+          get name() {
+            return this._name;
+          },
+          doWork: function() {
+            return "free";
+          },
+          toString: function() {
+            return this.name;
+          }
+        }, {});
+      }());
+      var Employee = (function($__super) {
+        function Employee(title, name) {
+          $traceurRuntime.superConstructor(Employee).call(this, name);
+          this.title = title;
+        }
+        return ($traceurRuntime.createClass)(Employee, {
+          doWork: function() {
+            return "paid";
+          },
+          set title(newValue) {
+            if (newValue) {
+              this._title = newValue;
+            }
+          },
+          get title() {
+            return this._title;
+          }
+        }, {}, $__super);
+      }(Person));
+      var e1 = new Employee("Developer", "Scott");
+      var p1 = new Person("Alex");
+      expect(p1.doWork()).toBe("free");
+      expect(e1.doWork()).toBe("paid");
+      expect(p1.toString()).toBe("Alex");
+      expect(e1.toString()).toBe("Scott");
+      var makeEveryoneWork = function() {
+        for (var people = [],
+            $__1 = 0; $__1 < arguments.length; $__1++)
+          people[$__1] = arguments[$__1];
+        var result = [];
+        for (var i = 0; i < people.length; i++) {
+          if (people[i] instanceof Person) {
+            result.push(people[i].doWork());
+          }
+        }
+        ;
+        return result;
+      };
+      expect(makeEveryoneWork(p1, e1, {})).toEqual(["free", "paid"]);
     });
   });
   return {};
--- starter_with_traceur/app/es6/inheritance.js
+++ starter_with_traceur/app/es6/inheritance.js
@@ -66,7 +66,7 @@ describe('inheritance', function() {
 			}
 
 			doWork(){
-				return `${this.name} is working ${this.title}`;
+				return `${this.name} is a working ${this.title}`;
 			}
 
 			set title(newValue) {
@@ -85,11 +85,87 @@ describe('inheritance', function() {
 
 		expect(e1.name).toBe("Scott");
 		expect(e1.title).toBe("Developer");
-		expect(e1.doWork()).toBe("Scott is working Developer")
+		expect(e1.doWork()).toBe("Scott is a working Developer")
 
 		e1.title = "Manager";
 		e1.name = "Christopher"
-		expect(e1.doWork()).toBe("Christopher is working Manager")
+		expect(e1.doWork()).toBe("Christopher is a working Manager")
+
+	});
+
+	it('can overide methods in super class', function() {
+
+
+		class Person {
+
+			constructor(name) {
+			   this.name = name;
+			}
+
+			set name(newValue) {
+				if(newValue) {
+					this._name = newValue;
+				}
+			}
+
+			get name() {
+				return this._name;
+			}
+
+			doWork(){
+				return "free";
+			}
+
+
+			toString() {
+				return this.name;
+			}
+
+		}
+
+		class Employee extends Person{
+
+			constructor(title,name){
+				super(name);
+				this.title = title;
+			}
+
+			doWork(){
+				return "paid";
+			}
+
+			set title(newValue) {
+				if(newValue){
+					this._title = newValue;
+				}
+			}
+
+			get title(){
+				return this._title;
+			}
+
+		}
+
+		let e1 = new Employee("Developer","Scott");
+		let p1 = new Person("Alex");
+
+		expect(p1.doWork()).toBe("free");
+		expect(e1.doWork()).toBe("paid");
+
+		expect(p1.toString()).toBe("Alex");
+		expect(e1.toString()).toBe("Scott");
+
+		let makeEveryoneWork = function (...people) {
+			var result = [];
+			for (var i = 0; i < people.length ; i++) {
+				if (people[i] instanceof Person) {
+					result.push(people[i].doWork());
+				}
+			};
+			return result;
+		}
+
+		expect(makeEveryoneWork(p1,e1,{})).toEqual(["free","paid"]);
 
 	});
 });
